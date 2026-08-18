ca9b7f523078086ca7e5bf5e34e961024de759e8
İbrahim Məmmədov <Ibrahim.Mammadov@nukhali.com>
2026-08-18 14:24:46 +0400

    refactor: rename getAllUsers to getAllCategories and update category data samples

--- src/main/java/com/rest_api/demo/category/controllers/CategoryController.java
+++ src/main/java/com/rest_api/demo/category/controllers/CategoryController.java
@@ -1,15 +1,25 @@
 package com.rest_api.demo.category.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/categories")
 public class CategoryController {
+
+    @Value("${spring.application.name:demo}")
+    private String appName;
+
     @GetMapping
-  public List<Map<String, Object>> getAllUsers() {
+    public List<Map<String, Object>> getAllCategories() {
         return List.of(
-            Map.of("id", 1, "name", "Ali", "app", appName),
-            Map.of("id", 2, "name", "Murad", "app", appName)
+            Map.of("id", 1, "name", "Texnologiya", "app", appName),
+            Map.of("id", 2, "name", "Dizayn", "app", appName)
         );
     }
 
